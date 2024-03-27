097528b668cf0e16c7f7b82c82760dde5d66da13
siendobry <siendobry@gmail.com>
2024-03-27 15:35:37 +0100

    Update entity ids to be of type long instead of integer

--- backend/clinic/src/main/java/com/esatto/clinic/model/Address.java
+++ backend/clinic/src/main/java/com/esatto/clinic/model/Address.java
@@ -10,7 +10,7 @@ public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String street;
 
@@ -30,11 +30,11 @@ public class Address {
         this.patient = patient;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- backend/clinic/src/main/java/com/esatto/clinic/model/Patient.java
+++ backend/clinic/src/main/java/com/esatto/clinic/model/Patient.java
@@ -10,7 +10,7 @@ public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String firstName;
 
@@ -31,11 +31,11 @@ public class Patient {
         this.address = address;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
